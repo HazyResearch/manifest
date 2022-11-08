dca8bbf96a1344ec64af9d3617701f1c9fb838fb
Laurel <lorr1@cs.stanford.edu>
2022-11-08 04:52:13 +0000

    [chore] fix hf api test

--- tests/test_huggingface_api.py
+++ tests/test_huggingface_api.py
@@ -229,7 +229,7 @@ def test_batch_encdec_generate():
     assert result[0][0] == " green sky"
     assert math.isclose(round(result[0][1], 3), -13.538)
     assert result[1][0] == " blue sky"
-    assert math.isclose(round(result[1][1], 3), -41.504)
+    assert math.isclose(round(result[1][1], 3), -41.503)
 
     # Truncate max length
     model.pipeline.max_length = 5
